c2542b6953b46454d552a5cbe9cfc3eb28d6a631
Satoshi <no@e.mail>
2024-10-09 12:06:24 +0900

    better docs

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,34 @@
 import type { ActionReturn } from 'svelte/action'
 
 interface Parameter {
+	/** かなを入力するinput */
 	kanaInput: HTMLInputElement
+	/**
+	 * カタカナに変換するか
+	 * @defaultValue false
+	 *  */
 	katakana?: boolean
 }
 
+/**
+ * ふりがなを自動で入力するためのSvelte Action
+ *
+ * @param node - かなを取得する元のinput要素
+ * @param options - Actionオプション
+ * @param options.kanaInput - 読み仮名を入力するinput要素
+ * @param options.katakana - 読み仮名をカタカナで出力するか (デフォルト: `false`)
+ *
+ * @example
+ * ```svelte
+ * <form>
+ * 	<label for="name">名前</label>
+ * 	<input id="name" type="text" use:autoKana={{ kanaInput }} />
+ *
+ * 	<label for="nameKana">名前（かな）</label>
+ * 	<input id="nameKana" type="text" bind:this={kanaInput} />
+ * </form>
+ * ```
+ */
 export function autoKana(
 	node: HTMLInputElement,
 	{ kanaInput, katakana = false }: Parameter
